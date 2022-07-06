5c8315d1be9002e0df82d09081630a2efb5aac83
Ilya <iii@ya.ru>
2022-07-06 21:39:16 +0500

    task 19 done

---
+++ Num19/Program.cs
@@ -0,0 +1,15 @@
+﻿string PaliCheck(int num1){
+    int first = num1/10000;
+    int second = (num1/1000)%10;
+    int fourth = (num1/10)%10;
+    int fifth = num1%10;
+    if (first == fifth && second == fourth) {return "Число является палиндромом";}
+    else return "Число не является палиндромом";
+}
+
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int lng = Convert.ToString(num).Length;
+
+if (lng != 5) {Console.WriteLine("Вы ввели не пятизначное число");}
+else Console.WriteLine(PaliCheck(num));
